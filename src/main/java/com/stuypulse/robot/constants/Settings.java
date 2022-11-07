17b45a77788a5dc1e982be76bb78408a1790f572
Oblivion409 <ben@goldfisher.com>
2022-11-07 15:03:29 -0500

    Make field offset center of field2d

--- src/main/java/com/stuypulse/robot/constants/Settings.java
+++ src/main/java/com/stuypulse/robot/constants/Settings.java
@@ -29,7 +29,7 @@ public interface Settings {
     }
 
     public interface Field {
-        Transform2d FIELD_OFFSET = new Transform2d(new Translation2d(0,0), new Rotation2d(0) );
+        Transform2d FIELD_OFFSET = new Transform2d(new Translation2d(8, 4), new Rotation2d(0));
     }
 
     public interface Romi {
