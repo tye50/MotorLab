e22d7cd546777332c867166cc51634d78de29894
Tracy Ye <tye50@stuy.edu>
2023-11-17 14:41:19 -0500

    scr->java->commands->drivefunctions.java

--- src/main/java/com/stuypulse/robot/commands/DriveFunctions.java
+++ src/main/java/com/stuypulse/robot/commands/DriveFunctions.java
@@ -2,27 +2,93 @@ package com.stuypulse.robot.commands;
 
 import com.stuypulse.robot.util.Motor;
 
+// ctr shift p --> WPILIB: build robot code --> WPILIB: simulate robot code --> make sure to check all the boxes
+
 public class DriveFunctions {
     
-    public static void driveForwards(Motor left, Motor right) {}
+    public static void driveForwards(Motor left, Motor right) {
+        left.set(1.0);
+        right.set(1.0);
+    }
 
-    public static void driveBackwards(Motor left, Motor right) {}
+    public static void driveBackwards(Motor left, Motor right) {
+        left.set(-1.0);
+        right.set(-1.0);
+    }
 
-    public static void turnRight(Motor left, Motor right) {}
+    public static void turnRight(Motor left, Motor right) {
+        // make one wheel turn and the other stays 
+        left.set(1.0);
+        right.set(0.0);
+    }
 
-    public static void turnLeft(Motor left, Motor right) {}
+    public static void turnLeft(Motor left, Motor right) {
+        left.set(0.0);
+        right.set(1.0);
+    }
 
-    public static void arcRight(Motor left, Motor right) {}
+    public static void arcRight(Motor left, Motor right) {
+        left.set(1.0);
+        right.set(0.5);
+    }
 
-    public static void arcLeft(Motor left, Motor right) {}
+    public static void arcLeft(Motor left, Motor right) {
+        left.set(0.5);
+        right.set(1.0);
+    }
 
-    public static void stopDistance(Motor left, Motor right) {}
+    public static void stopDistance(Motor left, Motor right) {
+        double setpoint = 60.0;
+        left.set(1.0);
+        right.set(1.0);
+        if (left.getDistance() >= setpoint) {
+            left.set(0.0);
+            right.set(0.0);
+        }
+    }
 
-    public static void bangBang(Motor left, Motor right) {}
+    public static void bangBang(Motor left, Motor right) {
+        double setpoint = 60.0;
+        // if statements are always running so move forward has to 
+        // be in the else
+        if (left.getDistance() >= setpoint) {
+            left.set(-1.0);
+            right.set(-1.0);
+        }
+        else {
+            left.set(1.0);
+            right.set(1.0);
+        }
+    }
 
-    public static void lessBang(Motor left, Motor right) {}
+    public static void lessBang(Motor left, Motor right) {
+        double setpoint = 60.0;
+        if (left.getDistance() < setpoint/2) {
+            left.set(1.0);
+            right.set(1.0);
+        }
+        else if (left.getDistance() >= setpoint/2 && left.getDistance() <= setpoint) {
+            left.set(0.5);
+            right.set(0.5);
+        }
+        else {
+            left.set(-1.0);
+            right.set(-1.0);
+        }
+    }
 
-    public static void betterControl(Motor left, Motor right) {}
+    public static void betterControl(Motor left, Motor right) {
+        double setpoint = 60.0;
+        double measurement = left.distance();
+        double error = setpoint - measurement; // error decreases over time
 
-    public static void bestestControl(Motor left, Motor right) {}
+        double speed = error/setpoint;
+        left.set(speed);
+        right.set(speed);
+        
+    }
+
+    public static void bestestControl(Motor left, Motor right) {
+        return;
+    }
 }
